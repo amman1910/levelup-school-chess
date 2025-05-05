7f2aaa128be1e8f39b1bef94aeb9cf0ef558a9ab
amman1910 <amirmn@post.jce.ac.il>
2025-05-05 11:38:15 +0300

    refactor: update Firebase configuration to use environment variables

--- chess-app/src/firebase.js
+++ chess-app/src/firebase.js
@@ -1,22 +1,17 @@
-// firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDANF7YOgHTRWPS-EIXWCWnB0HVYMHclaU",
-  authDomain: "level-up-chess.firebaseapp.com",
-  projectId: "level-up-chess",
-  storageBucket: "level-up-chess.appspot.com",
-  messagingSenderId: "10963404824",
-  appId: "1:10963404824:web:cc6572e48f0f0111fa9b6d",
-  measurementId: "G-1LVQ2TGN23"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// אתחול Firebase
 const app = initializeApp(firebaseConfig);
-
-// יצירת החיבור ל-Firestore
 const db = getFirestore(app);
 
-// ייצוא ה-Firestore
 export { db };
